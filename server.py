c8a2898ec763d4316df3d07c7be11d7f755fcca0
Matthew Sheydwasser <mattsheydwasser@gmail.com>
2023-01-27 13:03:34 -0700

    Added name to license

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import socketserver
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2023 Matthew Sheydwasser
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
